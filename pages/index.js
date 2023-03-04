248671264481f822ce1bb0b5a0bf0ec8aafce039
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-04 08:56:26 +0700

    Update:bug

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
               </div>
             </div>
           </section>
-          {/* <About /> */}
+          <About />
           <Feature />
         </main>
       </RootLayout>

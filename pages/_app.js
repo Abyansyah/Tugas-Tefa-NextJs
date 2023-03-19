44a3d9b51f68741e681288ea0397f05e85f7afc7
Ahmad Abyansyah <ahmadaby66@gmail.com>
2023-03-19 14:36:41 +0700

    Update:Nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
 import 'ahmad/styles/globals.css';
-import { CartProvider } from 'ahmad/hooks/cartcontexts';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <CartProvider>
-        <Component {...pageProps} />
-      </CartProvider>
+      <Component {...pageProps} />
     </>
   );
 }
